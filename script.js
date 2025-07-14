4b9388bf8d8cd2d4ee510b958fa142558f5cfa7e
ImmortallSoul <88667284+ImmortallSoul@users.noreply.github.com>
2025-07-14 20:55:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+fetch("taxi-options.html")
+    .then(res => res.text())
+    .then(html => {
+        document.getElementById("taxi-options-container").innerHTML = html;
+    });
+
 document.addEventListener("DOMContentLoaded", () => {
     const orderButton = document.querySelector("button");
 
