a0da85f4f1dc3faa62dfae8c7ade5fd2b6cf6487
Belak32 <chernenko.yrik@gmail.com>
2025-07-14 20:48:42 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const orderButton = document.querySelector("button");
+
+    orderButton.addEventListener("click", () => {
+        alert("Дякуємо! Ваше замовлення прийнято.");
+    });
+});
